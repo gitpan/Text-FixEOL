1935595e78e25202fd90b4c7657b8cb3a0ee0a04
Benjamin Franz <snowhare@nihongo.org>
2005-05-04 12:54:32 +0000

    Import of SNOWHARE/Text-FixEOL-1.01 from CPAN.
    gitpan-cpan-distribution: Text-FixEOL
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         SNOWHARE/Text-FixEOL-1.01.tar.gz
    gitpan-cpan-author:       SNOWHARE
    gitpan-cpan-maturity:     released

--- lib/Text/FixEOL.pm
+++ lib/Text/FixEOL.pm
@@ -3,7 +3,7 @@ package Text::FixEOL;
 use strict;
 use warnings;
 
-our $VERSION = '1.00';
+our $VERSION = '1.01';
 
 ##########################################################################################
 
